8076e7bf54a8c8aca3bddf8b15e5f072f9512241
rawatapurva <rawatapurva908@gmail.com>
2025-09-13 19:50:51 +0530

    Fix nodemailer config for production

--- Server/utils/mailSender.js
+++ Server/utils/mailSender.js
@@ -4,6 +4,8 @@ const mailSender = async (email, title, body) => {
     try{
         let transporter = nodemailer.createTransport({
             host:process.env.MAIL_HOST,
+            port: 465,             // ✅ required for Gmail SSL
+            secure: true,          // ✅ true for port 465
             auth:{
                 user:process.env.MAIL_USER,
                 pass:process.env.MAIL_PASS
